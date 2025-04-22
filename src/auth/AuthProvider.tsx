44b86dc4a65e5e187a3c29b17a896434506e56de
= <=>
2025-04-22 22:11:39 +0530

    added security token

--- src/auth/AuthProvider.tsx
+++ src/auth/AuthProvider.tsx
@@ -1,5 +1,5 @@
-import React, { createContext, useState } from 'react'
-
+import React, { createContext, useEffect, useState } from 'react'
+import * as keyChain from 'react-native-keychain';
 interface userVIew {
     firstName: string;
     lastName: string;
@@ -16,22 +16,59 @@ interface AuthContextType {
 export const AuthContext = createContext<AuthContextType | null>(null);
 
 import { ReactNode } from 'react';
+import { Alert } from 'react-native';
 
 export default function AuthProvider({ children }: { children: ReactNode }) {
     const [user, setUser] = useState<userVIew | null>(null);
-    function login(user: userVIew) {
-        if(user.email == "Venna" && user.password == "def")
+    const [token,setToken] = useState("");
+    let getUserData = async () => {
+        try
         {
-            setUser(() => ({
-                firstName: "venna",
-                lastName: "chandra ",
-                email: "abc",
-                password: "ddf",
-            }));
+            const creds = await keyChain.getGenericPassword();
+            if(creds) {
+                setToken(creds.password);
+                setUser(() => ({
+                    firstName: "venna",
+                    lastName: "chandra ",
+                    email: "abc",
+                    password: "",
+                }));
+            }
+        }
+        catch(err) {
+            Alert.alert("Failed to load token");
         }
     }
-    function logout() {
-        setUser(null);
+    useEffect(()=> {
+        getUserData();
+    },[]);
+    let login = async (user: userVIew) => {
+        try
+        { 
+            if(user.email == "Venna" && user.password == "def")
+            {
+                await keyChain.setGenericPassword("auth","dummyToken");
+                getUserData();
+            }
+            else
+            {
+                Alert.alert("login failed");
+            }
+        }
+        catch(err) {
+            Alert.alert("Login failed");
+        }
+    }
+    let logout = async () => {
+        try
+        {
+            setToken("");
+            setUser(null);
+            await keyChain.resetGenericPassword();
+        }
+        catch(err) {
+            Alert.alert(String(err));
+        }
     }
     return (
         <AuthContext.Provider value={{user,login,logout}}>
